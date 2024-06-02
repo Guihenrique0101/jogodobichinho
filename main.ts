be69dd9933cc5d06b6c977b2b48ac3b1e6bada1e
Guihenrique0101 <123760028+Guihenrique0101@users.noreply.github.com>
2024-06-02 16:01:48 -0300

    Atualizar 'main.blocks, main.ts'

--- main.ts
+++ main.ts
@@ -1,11 +1,11 @@
 input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Sad)
 })
-input.onGesture(Gesture.Shake, function () {
-    basic.showIcon(IconNames.Angry)
-})
 input.onButtonPressed(Button.AB, function () {
     basic.showIcon(IconNames.Happy)
+})
+input.onGesture(Gesture.FreeFall, function () {
+    basic.showIcon(IconNames.Angry)
 })
 input.onButtonPressed(Button.B, function () {
     basic.showIcon(IconNames.Heart)
